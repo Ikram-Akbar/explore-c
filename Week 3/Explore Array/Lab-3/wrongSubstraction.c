aecd960d424f4df65a02c9b3c2ede82304c3eadf
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-01-31 14:57:40 +0600

    wrong substraction and team

---
+++ Week 3/Explore Array/Lab-3/wrongSubstraction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i, n, k;
+    scanf("%d %d", &n, &k);
+    for (i = 0; i < k; i++)
+    {
+        if (n % 10 == 0)
+            n = n / 10;
+        else
+            n--;
+    }
+    printf("%d", n);
+}
