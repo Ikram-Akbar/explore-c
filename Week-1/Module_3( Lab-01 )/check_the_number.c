76e5bccfb1c035965f5e6779d93d61e789ba197a
ikram akbar <ikramakbar71@gmail.com>
2024-02-04 23:07:48 +0600

    find the large number

---
+++ Week-1/Module_3( Lab-01 )/check_the_number.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Please enter a number : \n");
+    scanf("%d", &d);
+}
---
+++ Week-1/Module_3( Lab-01 )/find_the_height.c
@@ -0,0 +1,16 @@
+// 1-130 - dwarf
+// 131-160 -> Normal
+// 161-190 -- > tall
+// 191--- inf --> abnormall
+
+#include<stdio.h>
+int main()
+{
+    int height;
+    printf("Please input your Height in in CM : \n");
+    scanf("%d", &height);
+    if(height<=0)
+    {
+
+    }
+}
