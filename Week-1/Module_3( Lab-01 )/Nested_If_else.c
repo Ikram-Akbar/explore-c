3f1648ab165e14e3684a8f51f7ad697d08c3593e
ikram akbar <ikramakbar71@gmail.com>
2024-02-06 23:19:31 +0600

    week 1 completed

--- Week-1/Module_3( Lab-01 )/Nested_If_else.c
+++ Week-1/Module_3( Lab-01 )/Nested_If_else.c
@@ -6,8 +6,7 @@ int main()
     scanf("%d", &first_number);
     printf("enter the second number : \n");
     scanf("%d", &second_number);
-    printf("enter the third number : \n");
-    scanf("%d", &third_number);
+
     if (first_number > second_number)
     {
         printf("First Number is Largest Number.");
